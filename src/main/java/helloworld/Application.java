1c09e825242b78ac7ba5c8e2bd00e6812aff33d8
Chia Chang <chia.chang@ge.com>
2016-12-17 13:07:22 -0800

    Adding rest of the functions.

--- src/main/java/helloworld/Application.java
+++ src/main/java/helloworld/Application.java
@@ -2,6 +2,8 @@ package helloworld;
 
 //import com.ge.ec;
 import com.ge.ec.ECClientImpl;
+import java.io.IOException;
+import java.util.HashMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,10 +13,13 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(Application.class, args);
         ECClientImpl ec=new ECClientImpl();
         String ver=ec.Version();
-        //ec.ECClient ec=new ec.ECClient();
+        Boolean _ref1=ec.Start();
+        Boolean _ref2=ec.Stop();
+        HashMap _hm=ec.getSettings();
+        ec.setSettings(_hm);
     }
 }
