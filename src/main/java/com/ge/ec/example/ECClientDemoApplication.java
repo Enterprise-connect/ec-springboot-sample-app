7e5e4fc0bf92f8cf7933bcea3851ca440fad4a32
Chia Chang <chia.chang@ge.com>
2016-12-18 21:28:42 -0800

    Adding sample updates.

--- src/main/java/com/ge/ec/example/ECClientDemoApplication.java
+++ src/main/java/com/ge/ec/example/ECClientDemoApplication.java
@@ -1,13 +1,12 @@
 package com.ge.ec.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.ge.ec.ECClientImpl;
+import java.io.IOException;
+import java.util.HashMap;
 
 @SpringBootApplication
 public class ECClientDemoApplication {
@@ -20,8 +19,14 @@ public class ECClientDemoApplication {
 @RestController
 class ECClientDemoController {
     
-    @RequestMapping("/hello")
-    String hello() {
-        return "test";
+    @RequestMapping("/start")
+    String start() throws IOException {
+        ECClientImpl ec=new ECClientImpl();
+        String ver=ec.Version();
+        Boolean _ref1=ec.Start();
+        //Boolean _ref2=ec.Stop();
+        //HashMap _hm=ec.getSettings();
+        //ec.setSettings(_hm);
+        return "EC Client is at your service.";
     }
 }
